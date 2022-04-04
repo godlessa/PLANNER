792709a215de626e18456eb19ed28552030855b5
DESKTOP-F00TJRJ\Irina <milesvia>
2022-04-04 18:08:40 +0300

    work on the error with customView

--- app/src/main/java/com/example/planner/CustomCalendarView.kt
+++ app/src/main/java/com/example/planner/CustomCalendarView.kt
@@ -2,25 +2,55 @@ package com.example.planner
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.CalendarView
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatViewInflater
+import com.example.planner.databinding.CalendarLayoutBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
-class CustomCalendarView(context: Context?, attrs: AttributeSet?) : LinearLayout(context, attrs) {
-    val planner: Calendar = Calendar.getInstance(Locale.ENGLISH)
-    val cnxt: Context? = context
-    val dateFormat: SimpleDateFormat = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+open class CustomCalendarView @JvmOverloads constructor
+    (context: Context,
+     attrs: AttributeSet? = null,
+     defStyleAttr: Int = 0,
+     defStyleRes: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+    private lateinit var binding: CalendarLayoutBinding
+    private var planner: Calendar = Calendar.getInstance(Locale.ENGLISH)
+    private var dateFormat: SimpleDateFormat
+
+    init{
+        dateFormat = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH);
+        binding = CalendarLayoutBinding.bind(binding.root)
+        setupViews()
+        setUpCalendar()
+    }
     val monthFormat: SimpleDateFormat = SimpleDateFormat("MMMM", Locale.ENGLISH);
     val yearFormat: SimpleDateFormat = SimpleDateFormat("yyyy", Locale.ENGLISH);
+
+
+
 
     var dates: List<Date> = mutableListOf()
     var eventsList: List<Event> = mutableListOf()
 
-    override fun equals(other: Any?): Boolean {
-        return super.equals(other)
+    private fun setupViews() {
+        with(binding) {
+            butPreviousMounth.setOnClickListener {
+                planner.add(Calendar.MONTH, -1)
+                setUpCalendar()
+            }
+            butNextMounth.setOnClickListener{
+                planner.add(Calendar.MONTH, 1)
+                setUpCalendar()
+        }
     }
 
-    private fun setupViews(){
+    }
 
+    private fun setUpCalendar(){
+        val currentDate: String = dateFormat.format(planner.time)
+        binding.tvCurrentDate.text = currentDate
     }
 }
