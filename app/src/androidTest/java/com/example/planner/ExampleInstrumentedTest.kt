f1826d094045edf91c9be68cfbdc3131cc39eb99
DESKTOP-F00TJRJ\Irina <milesvia>
2022-05-27 16:53:35 +0300

    test was created

--- app/src/androidTest/java/com/example/planner/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/planner/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.planner
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.planner.presentation.main.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Rule
+    @JvmField
+    var mainActivityActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.planner", appContext.packageName)
+    }
+
+    @Test
+    fun onClick() {
+        onView(withId(R.id.but_next_mounth)).perform(click()).check(matches(isDisplayed()))
     }
 }
---
+++ app/src/androidTest/java/com/example/planner/ExampleInstrumentedTestTest.kt
@@ -0,0 +1,5 @@
+package com.example.planner
+
+import junit.framework.TestCase
+
+class ExampleInstrumentedTestTest : TestCase()
